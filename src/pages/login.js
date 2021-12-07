158b9d531a1be69e59c4f46be6cb42c5df411e3b
Anthony Daniel <wowemotionscr@gmail.com>
2021-12-07 07:41:05 -0600

    Feature: Login and Register

---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+const Login = () => {
+  return <>Login</>;
+};
+export default Login;
---
+++ src/pages/register.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+const Register = () => {
+  return <>Register</>;
+};
+export default Register;
